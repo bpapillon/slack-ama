85dc6df9da7cb9c6a53182034e08a2a4e0929ec4
Ben Papillon <bpapillon@umbel.com>
2015-08-29 16:29:55 -0500

    remove python-dotenv; validate SLACK_COMMAND_TOKEN

--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,22 @@
-from dotenv import load_dotenv
-from flask import Flask, request
+from flask import abort, Flask, request
 from os import environ as env
 from slacker import Slacker
 
 app = Flask(__name__)
-load_dotenv('.env')
 slack = Slacker(env.get('SLACK_API_TOKEN'))
-
 
 @app.route('/question', methods=['POST'])
 def question():
+    valid_token = env.get('SLACK_COMMAND_TOKEN')
+    if valid_token:
+        request_token = request.form.get('token')
+        if request_token != valid_token:
+            abort(401)
     channel = env.get('SLACK_CHANNEL')
     text = request.form.get('text')
     if text:
         slack.chat.post_message(channel, text)
-
+    return 'ok'
 
 if __name__ == '__main__':
     app.run()
